47e9264fba317fe8d5039c65511d754813fc22e8
tomislav krtalic <99202100+Tomo188@users.noreply.github.com>
2023-02-23 10:08:44 +0000

    add data

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
+jobs = [{
+  "id": 1,
+  "job name": "Data Scientist",
+  "location": "Zagreb Croatia",
+  "salary": 5000
+}, {
+  "id": 2,
+  "job name": "Front End Developer",
+  "location": "Zagreb Croatia",
+  "salary": 2000
+}, {
+  "id": 3,
+  "job name": "Backend Developer",
+  "location": "Split Croatia",
+  "salary": 15000
+}, {
+  "id": 4,
+  "job name": "ML Scientist",
+  "location": "Zagreb Croatia",
+  "salary": 5000
+}]
 app = Flask(__name__)
 
 
 @app.route("/")
 def helloWorld():
-  return render_template("home.html")
+  return render_template("home.html", jobs=jobs)
 
 
 if __name__ == "__main__":
